2026007f76db20dff085581cda2908540af96ad4
Aaditi Menon <aaditi.menon.btech2023@sitnagpur.siu.edu.in>
2025-11-10 11:18:34 +0530

    Add TCP socket client implementation

---
+++ Practical2/tcp_socket_client.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 10001
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    const char *message = "Hello from client";
+
+    // Creating socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 address from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/Address not supported");
+        return -1;
+    }
+
+    // Connecting to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        return -1;
+    }
+
+    // Sending message
+    send(sock, message, strlen(message), 0);
+    printf("Message sent to server.\n");
+
+    // Reading response
+    read(sock, buffer, BUFFER_SIZE);
+    printf("Server says: %s\n", buffer);
+
+    // Closing
+    close(sock);
+    return 0;
+}
